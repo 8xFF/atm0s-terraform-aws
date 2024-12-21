5376f0af2adfea75da4c46d3abf2b83efdfb5af9
shadowWalker <hieu811@gmail.com>
2024-12-22 00:34:39 +0700

    fix: missing pass cluster secret to cluster module

--- clusters/outputs.tf
+++ clusters/outputs.tf
@@ -2,3 +2,8 @@ output "console_domain" {
   value       = module.ec2.console_public_dns
   description = "The console endpoint"
 }
+
+output "gateway_domain" {
+  value       = module.ec2.gateway_public_dns
+  description = "The of the gateway domain"
+}
